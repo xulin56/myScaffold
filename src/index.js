ae1a8cb140abe72bd480a9d2bb65c5eb7c16f917
xuxu <1132777020@qq.com>
2018-06-11 13:26:19 +0800

    完善rem

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import {Provider} from 'react-redux';
 import {store} from 'store';
 import {BrowserRouter, Route} from 'react-router-dom';
 import './index.scss';
+
+document.getElementsByTagName('html')[0].style.fontSize = window.screen.width / 37.5 +'px';
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
